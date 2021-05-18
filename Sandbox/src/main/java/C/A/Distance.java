bbf1c5a55bc0f60766ef547eea7df6f04fd2b36f
Ewelina K <ewelina.inka10@gmail.com>
2021-05-18 14:13:42 +0200

    Exercise 2 ver 2

---
+++ Sandbox/src/main/java/C/A/Distance.java
@@ -0,0 +1,17 @@
+package C.A;
+
+
+public class Distance {
+
+  public static void main (String[] args) {
+  Point p1= new Point(5,6);
+  Point p2= new Point(3,4);
+//distance(p1,p1);
+    //System.out.println("The distance between point A:( " + p1.x1 + "," + p1.y1 + " ) " + " and point B: ( " + p2.x2 + "," + p2.y2 + " ) is " + ab);
+
+
+ }
+
+ // private static void distance(Point p1, Point p2) {
+ // }
+}
