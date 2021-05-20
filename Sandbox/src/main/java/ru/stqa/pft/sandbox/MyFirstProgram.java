b507ec903eeac97a591c26577ef26a5feca19ac2
Ewelina K <ewelina.inka10@gmail.com>
2021-05-20 19:56:36 +0200

    Exercise 2 (points1-3)

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -14,6 +14,20 @@ public class MyFirstProgram {
 		Rectangle r= new Rectangle(4,6);
 
 		System.out.println("Area of the rectangle with side a " + r.a + " and b " + r.b + " = " + r.area());
+
+		Point p1=new Point(5,6);
+		Point p2=new Point(3,7);
+
+		System.out.println("The distance between point A:( " + p1.x + "," + p1.y + " ) " + " and point B: ( " + p2.x + "," + p2.y + " ) is " + distance(p1,p2));
+
+	}
+
+	public static double distance (Point p1, Point p2){
+		double dx = p2.x - p1.x;
+		double dy = p2.y - p1.y;
+		return Math.sqrt((dx * dx) + (dy * dy));
+
+
 	}
 
 	public static void hello(String somebody){
