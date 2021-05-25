9cc524e08a1d60c006231c226e75697787b5495b
Ewelina K <ewelina.inka10@gmail.com>
2021-05-25 14:37:11 +0200

    Exercise 4, point3 -Creating new contact

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDataContact.java
@@ -0,0 +1,67 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupDataContact {
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String emial1;
+  private final String emial2;
+  private final String emial3;
+  private final String homeno;
+  private final String mobileno;
+  private final String workno;
+  private final String faxno;
+
+  public GroupDataContact(String firstname, String lastname, String address, String emial1, String emial2, String emial3, String homeno, String mobileno, String workno, String faxno) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.emial1 = emial1;
+    this.emial2 = emial2;
+    this.emial3 = emial3;
+    this.homeno = homeno;
+    this.mobileno = mobileno;
+    this.workno = workno;
+    this.faxno = faxno;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getEmial1() {
+    return emial1;
+  }
+
+  public String getEmial2() {
+    return emial2;
+  }
+
+  public String getEmial3() {
+    return emial3;
+  }
+
+  public String getHomeno() {
+    return homeno;
+  }
+
+  public String getMobileno() {
+    return mobileno;
+  }
+
+  public String getWorkno() {
+    return workno;
+  }
+
+  public String getFaxno() {
+    return faxno;
+  }
+}
