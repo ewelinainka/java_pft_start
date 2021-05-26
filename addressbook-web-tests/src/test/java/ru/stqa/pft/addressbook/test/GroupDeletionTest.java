de4e5b71a2cc89957209ecc6a9eadca30f6a283f
Ewelina K <ewelina.inka10@gmail.com>
2021-05-26 16:14:28 +0200

    zamian dziedziczenia na delegowanie,, tworzenie dwupoziomowej architektury

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupDeletionTest.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.test;
 
 import org.testng.annotations.Test;
 
@@ -8,10 +8,10 @@ public class GroupDeletionTest extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
 
-    gotoGroupPage();
-    selectGroup();
-    deleteSelectedGroups();
-    returnToGroupPage();
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteSelectedGroups();
+    app.returnToGroupPage();
   }
 
 }
