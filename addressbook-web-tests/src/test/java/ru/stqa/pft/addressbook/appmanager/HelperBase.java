5de622180c37ba3723e170acbe31dd31462bff09
Ewelina K <ewelina.inka10@gmail.com>
2021-05-31 17:56:33 +0200

    Lesson 3 point 6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -19,8 +19,13 @@ public class HelperBase {
   public void type(By locator, String text) {
     click(locator);
     if (text != null) {
-      wd.findElement(locator).clear();
-      wd.findElement(locator).sendKeys(text);
+
+      String existingTest= wd.findElement(locator).getAttribute("value");
+      if(! text.equals(existingTest)){
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+
     }
   }
 
