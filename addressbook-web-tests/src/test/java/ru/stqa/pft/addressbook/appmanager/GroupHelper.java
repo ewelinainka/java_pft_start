a4cd92ba6657fe61cf4712d34b75bf504ec3f84e
Ewelina K <ewelina.inka10@gmail.com>
2021-05-31 12:45:58 +0200

    Update GroupHelper.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -29,27 +29,7 @@ public class GroupHelper extends HelperBase{
     click(By.xpath("(//input[@name='new'])[2]"));
   }
 
- /* public void submitContactCreation(By locator) {
-    click(locator);
-  }
 
-  public void fillContactForm(GroupDataContact groupDataContact) {
-    type(By.name("firstname"), groupDataContact.getFirstname());
-    type(By.name("lastname"), groupDataContact.getLastname());
-    type(By.name("address"), groupDataContact.getAddress());
-    type(By.name("email"), groupDataContact.getEmial1());
-    type(By.name("email2"), groupDataContact.getEmial2());
-    type(By.name("email3"), groupDataContact.getEmial3());
-    type(By.name("home"), groupDataContact.getHomeno());
-    type(By.name("mobile"), groupDataContact.getMobileno());
-    type(By.name("work"), groupDataContact.getWorkno());
-    type(By.name("fax"), groupDataContact.getFaxno());
-  }
-
-  public void addNewContact() {
-    click(By.linkText("add new"));
-  }
-*/
   public void deleteSelectedGroup() {
     click(By.name("delete"));
   }
@@ -65,16 +45,5 @@ public class GroupHelper extends HelperBase{
   public void submitGroupModification() {
     click(By.name("update"));
   }
-/*
-  public void selectContact() {
-    click(By.xpath("//input[@id='1']"));
-  }
 
-  public void editContact() {
-    click(By.xpath("//img[@alt='Edit']"));
-  }
-
-  public void deleteSelectedContact() {
-    click(By.xpath("//input[@value='Delete']"));
-  }*/
 }
