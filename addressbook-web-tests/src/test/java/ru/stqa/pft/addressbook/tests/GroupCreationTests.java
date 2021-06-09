76e67b6e023762793df94c012494100d82aaaa7c
Ewelina K <ewelina.inka10@gmail.com>
2021-06-09 20:45:34 +0200

    Lekcja 4 punkt 9
    Wartosci lambda (anonimowe funkcje) i elementy funkcjonalnego programowania

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,20 +15,24 @@ public class GroupCreationTests extends TestBase{
 
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupsHelper().getGroupList();
-    GroupData group= new GroupData("test q", null, null);
+    GroupData group= new GroupData("test qA", null, null);
 
     app.getGroupsHelper().createGroup(group);
 
     List<GroupData> after = app.getGroupsHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
+  /*  int max = 0;
     for( GroupData g :after){
       if(g.getId()>max){
         max = g.getId();
       }
-    }
-    group.setId(max);
+    }*/
+
+    //Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+  //  int max1 = after.stream().max((o1, o2)->Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+    group.setId(after.stream().max((o1, o2)->Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
