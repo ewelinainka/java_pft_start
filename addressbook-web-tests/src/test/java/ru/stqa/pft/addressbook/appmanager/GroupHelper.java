0cd012cb6d2163e17b2958176905aed52663446e
Ewelina K <ewelina.inka10@gmail.com>
2021-07-14 13:12:28 +0200

    Revert "Lekcja 4 punkt 4"
    This reverts commit 59351f636ea09f77b2904342ea8a0eaeba1cafbb.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -30,14 +30,11 @@ public class GroupHelper extends HelperBase{
 
 
   public void deleteSelectedGroup() {
-
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-
-    wd.findElements(By.name("selected[]")).get(index).click();
-   // click(By.name("selected[]"));
+  public void selectGroup() {
+    click(By.name("selected[]"));
   }
 
   public void initGroupModification() {
