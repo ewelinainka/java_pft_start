30131da1285e5c63ff8cdb29c2cb48d7fd55227e
Ewelina K <ewelina.inka10@gmail.com>
2021-07-14 13:11:47 +0200

    Revert "Lekcja 4 punkt 7"
    This reverts commit 147a7d4f2a0e5b3f553c1597d21d439428b68c84.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
   private final String name;
   private final String header;
   private final String footer;
@@ -12,15 +11,22 @@ public class GroupData {
     this.name = name;
     this.header = header;
     this.footer = footer;
-    this.id = null;
   }
 
-  public String getId() {//wygenerewane code>Getter po stworzeniu nowej zmiennej id i konstruktora
-    return id;
+  @Override //wygenerowana przez code> Generate> Equals - potrzebne zeby porownywalo/rozpoznawalo jako takie same elementy w liscie
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override //wygenerowana przez code> Generate> Equals - potrzebne zeby porownywalo/rozpoznawalo jako takie same elementy w liscie
+  public int hashCode() {
+    return Objects.hash(name);
   }
 
   public String getName() {
-
     return name;
   }
 
@@ -32,24 +38,10 @@ public class GroupData {
     return footer;
   }
 
-  @Override //wygenerewane code> equals
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
-  }
-
-  @Override //wygenerewane code> equals
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
-  @Override //wygenerewane code> toString
+  @Override//wygenerowana przez code> Generate> toString
   public String toString() {
     return "GroupData{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
+            "name='" + name + '\'' +
             '}';
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -15,24 +14,18 @@ public class GroupModificationTests extends TestBase{
     if(!app.getGroupsHelper().isThereAGroup()){
       app.getGroupsHelper().createGroup(new GroupData("test q", null, null));
     }
+   // int before = app.getGroupsHelper().getGroupCount();
 
     List<GroupData> before = app.getGroupsHelper().getGroupList();
     app.getGroupsHelper().selectGroup(before.size() -1);
     app.getGroupsHelper().initGroupModification();
-
-    //GroupData group = new GroupData(before.get(before.size() -1).getId(),"test 1a", "test2b", "test3c");
-    GroupData group = new GroupData("test 1a", "test2b", "test3c");
-
-    app.getGroupsHelper().fillGroupForm(group);
+    app.getGroupsHelper().fillGroupForm(new GroupData("test 1a", "test2b", "test3c"));
     app.getGroupsHelper().submitGroupModification();
     app.getGroupsHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupsHelper().getGroupList();
-
+    //int after = app.getGroupsHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() -1);
-    before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
