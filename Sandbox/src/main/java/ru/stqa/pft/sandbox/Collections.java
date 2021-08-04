36dfd4fcc4c83e4a36cd46f8dd8eed0794c9de3d
Ewelina K <ewelina.inka10@gmail.com>
2021-08-04 09:23:18 +0200

    Update Collections.java

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Collections {
-  public static void main(String[] args){
+  public static void main(String[] args) {
 
    /* String[] langs = new String[4];
     langs[0] = "Java";
@@ -13,7 +13,7 @@ public class Collections {
     langs[2] = "Python";
     langs[3] = "PHP"; */
 
-   String[] langs = {"Java", "C#", "Python", "PHP"}; //tablica
+    String[] langs = {"Java", "C#", "Python", "PHP"}; //tablica
   /*  for (int i=0; i< langs.length; i++){
       System.out.println("Chce nauczyc sie "+ langs[i]);
     }*/
@@ -24,8 +24,8 @@ public class Collections {
     languages.add("Python");*/
 
     List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
-    for (String l: langs){
-      System.out.println("Chce nauczyc sie "+ l);
+    for (String l : langs) {
+      System.out.println("Chce nauczyc sie " + l);
 
 
   /*  for (String l: langs){
@@ -33,5 +33,6 @@ public class Collections {
     }*/
 
 
+    }
   }
 }
