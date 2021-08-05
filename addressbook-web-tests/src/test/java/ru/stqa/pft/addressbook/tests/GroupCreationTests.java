d67215b224cc6632bc1f4c1317efd0139d7b24b2
Ewelina K <ewelina.inka10@gmail.com>
2021-08-05 07:01:44 +0200

    Lekcja 4 punkt 9 group creation test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,20 +14,22 @@ public class GroupCreationTests extends TestBase{
 
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupsHelper().getGroupList();
-    GroupData group = new GroupData( "test q", null, null);
+    GroupData group = new GroupData( "test qqq", null, null);
     app.getGroupsHelper().createGroup(group);
     List<GroupData> after =app.getGroupsHelper().getGroupList();
 
     Assert.assertEquals(after.size(), before.size() +1);
 
-    before.add(group);
-    int max = 0;
+   /*   int max = 0;
     for (GroupData g : after){
       if (g.getId() > max){
         max=g.getId();
       }
-    }
-    group.setId(max);
+    }*/
+  //  Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+   // int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals (new HashSet<Object>(before), new HashSet<Object>(after));
   }
