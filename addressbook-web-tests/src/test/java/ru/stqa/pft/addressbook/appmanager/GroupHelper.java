1defbe4ebeb0c2654e94530fb66edf84c606f65c
Ewelina K <ewelina.inka10@gmail.com>
2021-08-25 19:21:09 +0200

    Lekcja 5 punkt 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -4,14 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
-
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase{
 
-  public List<GroupData> list(){
-    List<GroupData> groups = new ArrayList<GroupData>();
+
+  public Set<GroupData> all(){
+    Set<GroupData> groups = new HashSet<GroupData>();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for(WebElement element : elements){
       String name = element.getText();
@@ -20,6 +21,7 @@ public class GroupHelper extends HelperBase{
     }
     return groups;
   };
+
   public GroupHelper(WebDriver wd) {
     super(wd);
   }
@@ -40,11 +42,13 @@ public class GroupHelper extends HelperBase{
   public void deleteSelectedGroup() {
     click(By.name("delete"));
   }
-  public void selectGroup(int index)
+
+  public void selectGroupById(int id)
   {
-    wd.findElements(By.name("selected[]")).get(index).click();
-   // click(By.name("selected[]"));
+    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
+
   }
+
   public void initGroupModification() {
     click(By.name("edit"));
   }
@@ -61,24 +65,24 @@ public class GroupHelper extends HelperBase{
     returnToGroupPage();
   }
 
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
     returnToGroupPage();
   }
 
-  public void delete(int index) {
-    selectGroup(index);
+
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroup();
     returnToGroupPage();
   }
 
 
-
-
-  public int getGroupCount() {
+    public int getGroupCount() {
    return wd.findElements(By.name("selected[]")).size();
   }
 }
+
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -2,8 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
-
-import java.util.List;
+import java.util.Set;
 
 public class GroupDeletionTest extends TestBase{
 
@@ -12,23 +11,21 @@ public class GroupDeletionTest extends TestBase{
   public void ensurePreconditions(){
     app.goTo().groupPage();
 
-    if(app.group().list().size() ==0){
+    if(app.group().all().size() ==0){
       app.group().create(new GroupData().withName("test1"));
     }
   }
   @Test
   public void testGroupDeletion() {
 
-    List<GroupData> before = app.group().list();
-    int index= before.size() - 1;
-    app.group().delete(index);
-
-    List<GroupData> after = app.group().list();
+    Set<GroupData> before = app.group().all();
+    GroupData deletedGroup = before.iterator().next();
+    app.group().delete(deletedGroup);
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() - 1);
 
-    before.remove(index);
-
-      Assert.assertEquals(before, after);
+    before.remove(deletedGroup);
+    Assert.assertEquals(before, after);
 
   }
 
